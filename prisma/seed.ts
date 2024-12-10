f7c2acf3284508b4a3a6351246bad72685436021
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-10 17:17:22 -0400

    Enhance seed script error handling and ensure proper disconnection from Prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,3 +12,12 @@ async function main() {
     });
     console.log('Departaments created')
 }
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    })
