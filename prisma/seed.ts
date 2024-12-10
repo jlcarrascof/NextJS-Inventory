f5f430a79bba043d4800da195558820ca61a10bf
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-10 17:16:06 -0400

    Add department seeding functionality to Prisma seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,4 +2,13 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-
+async function main() {
+    await prisma.department.createMany({
+        data: [
+            { name: 'Herramientas' },
+            { name: 'Pinturas' },
+            { name: 'Electricidad' },
+        ],
+    });
+    console.log('Departaments created')
+}
