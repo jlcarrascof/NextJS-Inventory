04795e0d3d3fb881d65a9c3e1c55b25601c78ad2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-14 20:20:20 -0400

    Add client-side rendering and conditional styling to Dashboard link for improved user experience

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ReactNode } from 'react'
 import { FaHome, FaList, FaBox } from 'react-icons/fa'
 import { usePathname } from 'next/navigation'
@@ -16,7 +18,8 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
         <nav>
           <ul className="space-y-2">
             <li>
-              <a href="/dashboard" className="flex items-center space-x-2 p-2 rounded hover:bg-green-400">
+              <a href="/dashboard"
+              className={`flex items-center space-x-2 p-2 rounded hover:bg-green-400 $pathname === '/dashboard' ? 'bg-green-400 text-white': ''}`}>
                 <FaHome />
                 <div className="px-2">
                   Home
