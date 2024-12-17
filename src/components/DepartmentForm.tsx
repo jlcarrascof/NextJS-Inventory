a99b2d389780aeca47a30838c1cac8dbf82446dc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-17 11:17:18 -0400

    Add DepartmentForm component for managing department data

---
+++ src/components/DepartmentForm.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { useState } from 'react'
+
+export default function DepartmentForm() {
+    const [name, setName] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
+}
