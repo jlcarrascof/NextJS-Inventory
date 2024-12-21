3d32379419d890ece328ab7907ecc04f2a5a8ec9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-21 10:23:17 -0400

    Add back navigation icon to DepartmentForm component

--- src/components/DepartmentForm.tsx
+++ src/components/DepartmentForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { FaDatabase, FaTimes } from 'react-icons/fa'
+import { FaDatabase, FaTimes, FaArrowLeft } from 'react-icons/fa'
 
 export default function DepartmentForm() {
     const [name, setName] = useState('')
