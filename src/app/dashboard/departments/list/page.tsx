3c63f35809867dcc1fd3323739e4dcbc88545eac
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-21 10:25:31 -0400

    Add DepartmentsListPage component to display department list

---
+++ src/app/dashboard/departments/list/page.tsx
@@ -0,0 +1,10 @@
+import DepartmentList from '@/components/DepartmentList'
+
+export default function DepartmentsListPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Departments</h1>
+      <DepartmentList />
+    </div>
+  )
+}
