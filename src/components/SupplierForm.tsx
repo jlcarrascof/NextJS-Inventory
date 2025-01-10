4af807d68e3e887b0778972a02caa0e44db30687
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-10 12:08:40 -0400

    Add Supplier interface to define supplier properties in SupplierForm component

--- src/components/SupplierForm.tsx
+++ src/components/SupplierForm.tsx
@@ -2,6 +2,14 @@
 import { useState } from 'react';
 import { FaDatabase, FaTimes } from 'react-icons/fa'
 
+interface Supplier {
+    name: string
+    contact: string
+    address: string
+    phone: string
+    country: string
+}
+
 export default function SupplierForm() {
 
 }
