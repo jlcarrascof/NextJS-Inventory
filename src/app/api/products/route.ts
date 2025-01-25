8fef35f6cfc82db378d25fd5f76823be4ad20235
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-25 12:23:30 -0400

    Implement product creation logic in POST endpoint with response handling

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -9,6 +9,12 @@ export async function POST(req: Request) {
         if (!name || !quantity || !price || !cost || !departmentId) {
             return new Response('Missing required fields', { status: 400})
         }
+
+        const product = await prisma.product.create({
+            data: { name, quantity, price, cost, departmentId, supplierId },
+        })
+
+        return Response.json(product, { status: 201 })
     } catch (error) {
         return new Response('Failed to create a Product', { status: 500 })
     }
