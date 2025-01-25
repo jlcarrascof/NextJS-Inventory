fa7fd6905f1609917252fb74e83e6afdd1964c28
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-25 14:46:09 -0400

    Add TypeScript interfaces for ProductForm inputs, departments, and suppliers

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -1,5 +1,25 @@
-'use client';
+'use client'
 
 import { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Combobox } from '@headlessui/react'
+
+interface ProductFormInputs {
+  name: string;
+  quantity: number;
+  price: number;
+  cost: number;
+  departmentId: number;
+  supplierId?: number;
+}
+
+interface Department {
+  id: number;
+  name: string;
+}
+
+interface Supplier {
+  id: number;
+  name: string;
+}
+
