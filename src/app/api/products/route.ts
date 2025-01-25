913734f7b0822997051c46ca7534ae1874611e53
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-25 12:22:49 -0400

    Add validation for required fields in product creation POST request

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,7 +4,11 @@ const prisma = new PrismaClient()
 
 export async function POST(req: Request) {
     try {
+        const { name, quantity, price, cost, departmentId, supplierId } = await req.json()
 
+        if (!name || !quantity || !price || !cost || !departmentId) {
+            return new Response('Missing required fields', { status: 400})
+        }
     } catch (error) {
         return new Response('Failed to create a Product', { status: 500 })
     }
