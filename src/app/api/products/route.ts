bcfbd288fb9d402cb45a26f49da5ab7b8f2d5737
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-25 13:19:51 -0400

    Add validation for department existence in product creation

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -13,15 +13,23 @@ export async function POST(req: Request) {
 
         // Advanced validations ...
         if (quantity <= 0) {
-            return new Response('Quantity must be greater than zero', { status: 400 });
+            return new Response('Quantity must be greater than zero', { status: 400 })
         }
 
         if (price <= 0 || cost <= 0) {
-            return new Response('Price and cost must be greater than zero', { status: 400 });
+            return new Response('Price and cost must be greater than zero', { status: 400 })
         }
 
         if (price < cost) {
-            return new Response('Price cannot be lower than cost', { status: 400 });
+            return new Response('Price cannot be lower than cost', { status: 400 })
+        }
+
+        // Verify if the department exists ...
+        const departmentExists = await prisma.department.findUnique({
+            where: { id: departmentId },
+        })
+        if (!departmentExists) {
+            return new Response('Invalid department ID', { status: 400 });
         }
 
         const product = await prisma.product.create({
