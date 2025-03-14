fd18807c6f382be993e4fc05a627622f16cbb165
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-13 21:05:14 -0400

    Add ProductsListPage component to display product list

---
+++ src/app/dashboard/products/list/page.tsx
@@ -0,0 +1,10 @@
+import ProductList from '@/components/ProductList'
+
+export default function ProductsListPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Products</h1>
+      <ProductList />
+    </div>
+  )
+}
